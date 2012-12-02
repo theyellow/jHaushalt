c7983f6496c9514e6ae9829cce01e54093bfae21
ohoogvliet <oliver@hoogvliet.de>
2012-12-02 20:09:19 +0100

    Test for second register

--- test/jhaushalt/service/factories/DatenbasisIntegrationTest.java
+++ test/jhaushalt/service/factories/DatenbasisIntegrationTest.java
@@ -36,21 +36,35 @@ public class DatenbasisIntegrationTest {
 		assertThat(datenbasis.getAnzahlAutoStandardBuchungen()).isEqualTo(0);
 		assertThat(datenbasis.getAnzahlAutoUmbuchungen()).isEqualTo(0);
 		List<Register> registers = datenbasis.getRegisterList();
-		assertThat(registers.size()).isEqualTo(1);
+		assertThat(registers.size()).isEqualTo(2);
 
-		Register neuRegister = registers.get(0); 
-		assertThat(neuRegister.getAnzahlBuchungen()).isEqualTo(4);
-		assertThat(neuRegister.getName()).isEqualTo("[NEU]");
-		List<Buchung> registerBookings = neuRegister.getBookings();
+		assertThatFirstRegisterIsSetCorrectly(registers.get(0));
+		assertThatSecondRegisterIsSetCorrectly(registers.get(1));
+	}
+
+	private void assertThatFirstRegisterIsSetCorrectly(Register register) { 
+		assertThat(register.getAnzahlBuchungen()).isEqualTo(4);
+		assertThat(register.getName()).isEqualTo("[Girokonto]");
+		List<Buchung> registerBookings = register.getBookings();
 		assertThat(registerBookings).hasSize(4);
 		
 		assertThatBookingEntryIsCorrect(registerBookings.get(0), Umbuchung.class, new Datum("16.11.12"), "Eröffnungssaldo", UmbuchungKategorie.class, new Geldbetrag("200000"));
 		assertThatBookingEntryIsCorrect(registerBookings.get(1), StandardBuchung.class, new Datum("17.11.12"), "Einzahlung", EinzelKategorie.class, new Geldbetrag("430000"));
 		assertThatBookingEntryIsCorrect(registerBookings.get(2), StandardBuchung.class, new Datum("18.11.12"), "Stromrechnung", EinzelKategorie.class, new Geldbetrag("-3467"));
-		assertThatBookingEntryIsCorrect(registerBookings.get(3), SplitBuchung.class, new Datum("02.12.12"), "Barauszahlung", MehrfachKategorie.class, new Geldbetrag("-30000"));
-
+		assertThatBookingEntryIsCorrect(registerBookings.get(3), SplitBuchung.class, new Datum("02.12.12"), "Barauszahlung", MehrfachKategorie.class, new Geldbetrag("-30000"));		
+	}
+	
+	private void assertThatSecondRegisterIsSetCorrectly(Register register) { 
+		assertThat(register.getAnzahlBuchungen()).isEqualTo(2);
+		assertThat(register.getName()).isEqualTo("[Sparkonto]");
+		List<Buchung> registerBookings = register.getBookings();
+		assertThat(registerBookings).hasSize(2);
+		
+		assertThatBookingEntryIsCorrect(registerBookings.get(0), Umbuchung.class, new Datum("02.12.12"), "Eröffnungssaldo", UmbuchungKategorie.class, new Geldbetrag("30014"));
+		assertThatBookingEntryIsCorrect(registerBookings.get(1), StandardBuchung.class, new Datum("02.12.12"), "Auszahlung", EinzelKategorie.class, new Geldbetrag("-5000"));
 	}
 
+	
 	private void assertThatBookingEntryIsCorrect(Buchung bookingEntry, Class<?> expectedBookingType, Datum expectedDate, String entryText, Class<?> expectedClassType, Geldbetrag expectedAmount) {
 		assertThat(bookingEntry).isInstanceOf(expectedBookingType);
 		assertThat(bookingEntry.getDatum()).isEqualTo(expectedDate);
